b1004e3b9e06efe7e953431ba004fd85e8b1ee91
patelaryan7751 <59426397+patelaryan7751@users.noreply.github.com>
2020-12-12 11:00:06 +0530

    added a link extractor

---
+++ js/teachass.js
@@ -0,0 +1,27 @@
+var us=document.getElementById("user");
+us.textContent=sessionStorage.getItem("emails");
+document.getElementById("createass").addEventListener("click",cretefass);
+function cretefass(){
+    
+     window.open('assigncreate.html','_self');
+
+}
+document.getElementById("createsub").addEventListener("click",cretefsub);
+function cretefsub(){
+    
+     window.open('submission.html','_self');
+
+}
+document.getElementById("home").addEventListener("click",cretefhom);
+function cretefhom(){
+    
+     window.open('submission.html','_self');
+
+}
+var url_string1=window.location.href;
+         sessionStorage.setItem("link",url_string1);
+          var url_string=decodeURI(url_string1);
+         console.log(url_string);
+var url= new URL(url_string);
+var roomname =url.searchParams.get("roomname");
+sessionStorage.setItem("roomi",roomname);
